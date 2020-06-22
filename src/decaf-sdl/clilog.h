8bf9111ec6b41e94fee52372e985ddfc4db4fbfe
Carl Kenner <carl.kenner@gmail.com>
2020-06-22 14:26:12 +0930

    Fix building in Dolphin

--- src/decaf-sdl/clilog.h
+++ src/decaf-sdl/clilog.h
@@ -1,5 +1,12 @@
 #pragma once
+#ifdef DOLPHIN
+#include <common/log.h>
+#include <fmt/format.h>
+extern std::shared_ptr<Logger>
+   gCliLog;
+#else
 #include <spdlog/spdlog.h>
-
 extern std::shared_ptr<spdlog::logger>
-gCliLog;
+   gCliLog;
+#endif
+
